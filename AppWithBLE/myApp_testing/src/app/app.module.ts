95f06b9a1977d3c1792a29c3d91cf606f8510708
LuisVila99 <42320808+LuisVila99@users.noreply.github.com>
2021-04-16 15:59:23 +0100

    app with randd plugin ble

---
+++ AppWithBLE/myApp_testing/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { BluetoothLE } from '@ionic-native/bluetooth-le/ngx';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, BluetoothLE],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
