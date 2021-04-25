3186d935805980f9cb4fbf5f2092795b54f72376
LuisVila99 <42320808+LuisVila99@users.noreply.github.com>
2021-04-25 19:27:55 +0100

    comentarios added

--- AppWithBLE/myApp_testing/src/app/home/home.page.ts
+++ AppWithBLE/myApp_testing/src/app/home/home.page.ts
@@ -6,20 +6,24 @@ import { BluetoothLE } from '@ionic-native/bluetooth-le/ngx';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage {
 
-  address: string = '';
   constructor(private bluetoothLE: BluetoothLE) {}
 
+  //--------------\\
+  // CENTRAL MODE \\
+  //--------------\\
   async central(){
     console.log('inicia central')
-    this.bluetoothLE.initialize().subscribe(); //inicia BLE
-    this.bluetoothLE.enable(); // enable BLE 
 
+    // INICIALIZA E ENABLE AO BLE 
+    this.bluetoothLE.initialize().subscribe();
+    this.bluetoothLE.enable();
+    var a1 = await this.bluetoothLE.getAdapterInfo(); // verifica ble enabled e inicializado
+    console.log(a1); 
 
-    var a1 = await this.bluetoothLE.getAdapterInfo();
-    console.log(a1); // verifica ble enabled e inicializado
-
+    // VERIFICA SE LOCALIZAÇÃO ESTÁ ATIVA
+    // CASO NÃO ESTEJA PEDE AO USER QUE A ATIVE COM UM POP-UP
     console.log('location enabled: ');
     console.log(await this.bluetoothLE.isLocationEnabled());
     this.bluetoothLE.hasPermission().then(res => {
@@ -29,55 +33,52 @@ export class HomePage implements OnInit{
       }
     })
 
+    // PARAMETROS A UTILIZAR NO SCAN
     var param_scan = {
-      "services": [],
+      "services": [], // não filtra nenhuns serviços, apanha tudo
       "allowDuplicates": true,
       "scanMode": this.bluetoothLE.SCAN_MODE_LOW_LATENCY,
       "matchMode": this.bluetoothLE.MATCH_MODE_AGGRESSIVE,
       "matchNum": this.bluetoothLE.MATCH_NUM_MAX_ADVERTISEMENT,
       "callbackType": this.bluetoothLE.CALLBACK_TYPE_ALL_MATCHES,
     }
-    
+    // INICIA SCANNING
     this.bluetoothLE.startScan(param_scan).subscribe(scanStatus => {
       if(scanStatus.status == 'scanStarted'){
         console.log('em scanning...');
       }
-      if(scanStatus.status == 'scanResult' && scanStatus.name == 'Hello World'){
-        console.log(scanStatus); // printa o que encontra
-        //this.stopScan(); //para o scan -> se o conect falhar, recomeçar a mao 
+      if(scanStatus.status == 'scanResult' && scanStatus.name == 'Hello World'){ //hardcoded para encontrar o peripheral 'Hello World'
+        console.log(scanStatus); // printa se encontrar
+        //this.stopScan(); //para o scan -> se o conect falhar, recomeçar a mao (dropped porque influencia o scanning)
+        
+        //CONECTAR À ADRESS ENCONTRADA (correspondente ao scanStatus.name == 'Hello World')
         var param = {
           address: scanStatus.address
         }
-
         this.bluetoothLE.connect(param).subscribe(status =>
           console.log(status)
         );
-
-        //this.bluetoothLE.disconnect(param).then(status =>
-        //  console.log(status)
-        //);
        }
     }); 
   }
 
 
-
-
-
-
-
+  //-----------------\\
+  // PERIPHERAL MODE \\
+  //-----------------\\
   async peripheral(){
     console.log('inicia peripheral');
-    this.bluetoothLE.initialize().subscribe(); //inicia BLE
-    this.bluetoothLE.enable(); // enable BLE 
 
+    // INICIALIZA E ENABLE AO BLE + INICIALIZA PERIPHERAL MODE
+    this.bluetoothLE.initialize().subscribe(); 
+    this.bluetoothLE.enable();
     var param = {
       "request": true,
       "restoreKey": "bluetoothleplugin"
     }
-    this.bluetoothLE.initializePeripheral(param).subscribe(); // inicia peripheral
+    this.bluetoothLE.initializePeripheral(param).subscribe();
 
-    // ADICIONAR SERVICES ???
+    // ADICIONAR SERVICES ??? -> como estava no readme do plugin
     var params = {
       service: "1234",
       characteristics: [
@@ -105,7 +106,7 @@ export class HomePage implements OnInit{
     var stat = await this.bluetoothLE.addService(params);
     console.log(stat);
 
-
+    // INICIA ADVERTISING COM SERVICE 1234 e define-se o nome Hello World
     var paramz = {
       "services":["1234"], //iOS
       "service":"1234", //Android
@@ -113,45 +114,16 @@ export class HomePage implements OnInit{
     }
     var status = await this.bluetoothLE.startAdvertising(paramz);
     console.log(status)
-
     var isAdv = await this.bluetoothLE.isAdvertising();
-    console.log(isAdv); // A ENVIAR ADV PACKETS CHECK
-
-
-
+    console.log(isAdv); // CONFIRMA QUE ESTÁ ADVERTISING 
 
     console.log('fim peripheral');
   }
 
-
-
-
+  
+  // FUNÇÃO PARA PARAR O SCAN DO CENTRAL
   async stopScan(){
     this.bluetoothLE.stopScan();
     console.log('parei scan');
-  }
-
-
-  //connect(){
-  //  var params = {
-  //    address: this.address
-  //  }
-  //  this.bluetoothLE.connect(params).subscribe();
-  //}
-
-  async ngOnInit() {
-    
-  }
-
-
-  async centralMode(){
-    //this.address='';
-    this.central();
-    //this.central().then(() =>{
-    //  if (this.address!=''){
-    //    console.log('here');
-    //    this.connect();
-    //  }
-    //});    
   }
 }
